9a35c8044e1c84cd24e1482742ae0b2d22de8cc5
morimichimaeno <morimichimaeno@gmail.com>
2018-03-22 16:32:33 +0900

    ScriptTestプロジェクト 課題
    「課題：配列を宣言して出力しましょう」です

---
+++ Assets/Task1.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Task1 : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        int[] array = {10, 20, 60, 50, 80};
+
+        //配列の各要素の値を順番に表示
+        //for (int i = 0; i <= array.Length; i++) {
+        //    Debug.Log(array[i]);
+        //}
+
+        //for文を使い、配列の各要素の値を逆順に表示
+        for (int j = array.Length - 1; j >= 0; j--) {
+            Debug.Log(array[j]);
+        }
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ Assets/Test.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Boss {
+    private int hp = 100;//体力
+    private int power = 25;//攻撃力
+    private int mp = 53;//魔力
+
+    //攻撃用の関数
+    public void Attack() {
+        Debug.Log(this.power + "のダメージを与えた");
+    }
+
+    //魔法攻撃の関数
+    public void Magic() {
+        if (mp > 4) {
+            this.mp -= 5;
+            Debug.Log("魔法攻撃をした。残りMPは" + this.mp);
+        } else {
+            Debug.Log("MPが足りないため魔法が使えない。");
+        }
+        
+    }
+
+    //防御用の関数
+    public void Defence(int damage) {
+        Debug.Log(damage + "のダメージを受けた");
+        //残りHPを減らす
+        this.hp -= damage;
+    }
+}
+
+public class Test : MonoBehaviour {
+
+    // Use this for initialization
+    void Start() {
+        //Bossクラスの変数を宣言してインスタンスを代入
+        Boss lastboss = new Boss();
+
+        //攻撃用の関数を呼び出す
+        lastboss.Attack();
+        //魔法攻撃の関数を呼び出す
+        lastboss.Magic();
+        for (int i = 0; i < 10; i++) {
+            lastboss.Magic();
+        }
+        //防御用の関数を呼び出す
+        lastboss.Defence(3);
+    }
+
+    // Update is called once per frame
+    void Update() {
+
+    }
+}
